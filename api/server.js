bb777677abc3bc3b2267cddaa5872131917ceb43
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-04-18 21:28:52 -0400

    add code

--- api/server.js
+++ api/server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const server = express();
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
 const port = 8080;
 
 const mvp = require('./mvp.json');
+
+const http = require('http');
+// const fs = require('fs');
+ 
+
+server.use(express.json());
+server.use(express.urlencoded({extended: false}));
+server.use(express.static('public'));
 
 // fs.readFile('index.html', (err, html) => {
 //         if(err){
 //             throw err;
 //         }
-    //     const server = http.createServer((req, res) => {
-    //         res.statusCode = 200;
-    //         res.setHeader('Content-type', 'test/html');
-    //         res.write(html);
-    //         res.end('');
-    // });
+//         const serverRender = http.createServer((req, res) => {
+//             res.statusCode = 200;
+//             res.setHeader('Content-type', 'test/html');
+//             res.write(html);
+//             res.end('');
+//     });
+// serverRender();
 
     server.get("/api/mvp/:year", (req, res) => {
         res.send(`mvps ${req.params.year}`)
